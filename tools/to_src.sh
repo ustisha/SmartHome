d08388025862769502f71b5cfd287e609daa5114
Dmitriy Deryabin <d.deryabin@cognitive.ru>
2021-02-22 19:06:36 +0700

    COMMON. Library update.

--- tools/to_src.sh
+++ tools/to_src.sh
@@ -5,9 +5,10 @@ if [ "$BASE_DIR" = "/" ]; then
   BASE_DIR=""
 fi
 
-U8G2LIB="$BASE_DIR/../U8g2lib"
-FROM1="$BASE_DIR/../U8g2lib/cppsrc"
-FROM2="$BASE_DIR/../U8g2lib/csrc"
+# Copy u8g2lib files to src, cmake load files from src directory.
+U8G2LIB="$BASE_DIR/../libraries/U8g2lib"
+FROM1="$BASE_DIR/../libraries/U8g2lib/cppsrc"
+FROM2="$BASE_DIR/../libraries/U8g2lib/csrc"
 SRC="$U8G2LIB/src"
 
 if [ ! -d $SRC ]; then
