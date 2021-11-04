5922bb736d8536027b09f3f4009599681ec4d1d0
Dmitriy Deryabin <dmitriy@ustisha.ru>
2021-11-04 18:21:23 +0700

    Update gateway commands.

--- esp/gateway/include/MqttTranslate.h
+++ esp/gateway/include/MqttTranslate.h
@@ -23,7 +23,7 @@ NetMap const components[COMPONENTS_LEN] = {
         {"main_display",   MAIN_DISPLAY},
 };
 
-const uint8_t PORTS_LEN = 35;
+const uint8_t PORTS_LEN = 40;
 NetMap const ports[PORTS_LEN] = {
         {"http_handler",  PORT_HTTP_HANDLER},
         {"info",          PORT_INFO},
@@ -49,6 +49,11 @@ NetMap const ports[PORTS_LEN] = {
         {"18b20_2",       PORT_18B20_2},
         {"18b20_3",       PORT_18B20_3},
         {"18b20_4",       PORT_18B20_4},
+        {"relay_1",       PORT_RELAY_1},
+        {"relay_2",       PORT_RELAY_2},
+        {"relay_3",       PORT_RELAY_3},
+        {"relay_4",       PORT_RELAY_4},
+        {"relay_5",       PORT_RELAY_5},
         {"value2",        PORT_VALUE_2},
         {"value3",        PORT_VALUE_3},
         {"value4",        PORT_VALUE_4},
@@ -64,35 +69,35 @@ NetMap const ports[PORTS_LEN] = {
 
 const uint8_t COMMANDS_LEN = 58;
 NetMap const commands[COMMANDS_LEN] = {
-        {"temperature",    CMD_TEMPERATURE},
-        {"humidity",       CMD_HUMIDITY},
-        {"pressure",       CMD_PRESSURE},
-        {"light",          CMD_LIGHT},
-        {"vcc",            CMD_VCC},
-        {"value",          CMD_VALUE},
-        {"get_values",     CMD_GET_VALUES},
-        {"mode",           CMD_MODE},
-        {"timeout",        CMD_TIMEOUT},
-        {"relay_00",       CMD_RELAY_00},
-        {"relay_01",       CMD_RELAY_01},
-        {"relay_02",       CMD_RELAY_02},
-        {"relay_03",       CMD_RELAY_03},
-        {"relay_04",       CMD_RELAY_04},
-        {"relay_on_00",    CMD_RELAY_ON_00},
-        {"relay_on_01",    CMD_RELAY_ON_01},
-        {"relay_on_02",    CMD_RELAY_ON_02},
-        {"relay_on_03",    CMD_RELAY_ON_03},
-        {"relay_on_04",    CMD_RELAY_ON_04},
-        {"relay_off_00",   CMD_RELAY_OFF_00},
-        {"relay_off_01",   CMD_RELAY_OFF_01},
-        {"relay_off_02",   CMD_RELAY_OFF_02},
-        {"relay_off_03",   CMD_RELAY_OFF_03},
-        {"relay_off_04",   CMD_RELAY_OFF_04},
-        {"servo_00",       CMD_SERVO_00},
-        {"servo_01",       CMD_SERVO_01},
-        {"servo_02",       CMD_SERVO_02},
-        {"servo_03",       CMD_SERVO_03},
-        {"servo_04",       CMD_SERVO_04},
+        {"temperature",        CMD_TEMPERATURE},
+        {"humidity",           CMD_HUMIDITY},
+        {"pressure",           CMD_PRESSURE},
+        {"light",              CMD_LIGHT},
+        {"vcc",                CMD_VCC},
+        {"value",              CMD_VALUE},
+        {"get_values",         CMD_GET_VALUES},
+        {"mode",               CMD_MODE},
+        {"timeout",            CMD_TIMEOUT},
+        {"relay_00",           CMD_RELAY_00},
+        {"relay_01",           CMD_RELAY_01},
+        {"relay_02",           CMD_RELAY_02},
+        {"relay_03",           CMD_RELAY_03},
+        {"relay_04",           CMD_RELAY_04},
+        {"relay_on_00",        CMD_RELAY_ON_00},
+        {"relay_on_01",        CMD_RELAY_ON_01},
+        {"relay_on_02",        CMD_RELAY_ON_02},
+        {"relay_on_03",        CMD_RELAY_ON_03},
+        {"relay_on_04",        CMD_RELAY_ON_04},
+        {"relay_off_00",       CMD_RELAY_OFF_00},
+        {"relay_off_01",       CMD_RELAY_OFF_01},
+        {"relay_off_02",       CMD_RELAY_OFF_02},
+        {"relay_off_03",       CMD_RELAY_OFF_03},
+        {"relay_off_04",       CMD_RELAY_OFF_04},
+        {"servo_00",           CMD_SERVO_00},
+        {"servo_01",           CMD_SERVO_01},
+        {"servo_02",           CMD_SERVO_02},
+        {"servo_03",           CMD_SERVO_03},
+        {"servo_04",           CMD_SERVO_04},
         {"servo_min_angle_00", CMD_SERVO_MIN_ANGLE_00},
         {"servo_min_angle_01", CMD_SERVO_MIN_ANGLE_01},
         {"servo_min_angle_02", CMD_SERVO_MIN_ANGLE_02},
@@ -108,20 +113,20 @@ NetMap const commands[COMMANDS_LEN] = {
         {"servo_ratio_02",     CMD_SERVO_RATIO_02},
         {"servo_ratio_03",     CMD_SERVO_RATIO_03},
         {"servo_ratio_04",     CMD_SERVO_RATIO_04},
-        {"up_limit",       CMD_UP_LIMIT},
-        {"down_limit",     CMD_DOWN_LIMIT},
-        {"rtc_date",       CMD_RTC_DATE},
-        {"rtc_time",       CMD_RTC_TIME},
-        {"moisture",       CMD_MOISTURE},
-        {"motion",         CMD_MOTION},
-        {"activity_ratio", CMD_ACTIVITY_RATIO},
-        {"activity_limit", CMD_ACTIVITY_LIMIT},
-        {"recall_ratio",   CMD_RECALL_RATIO},
-        {"recall_timeout", CMD_RECALL_TIMEOUT},
-        {"time_left",      CMD_TIME_LEFT},
-        {"call",           CMD_CALL},
-        {"energy_level",   CMD_ENERGY_LEVEL},
-        {"info",           CMD_INFO},
+        {"up_limit",           CMD_UP_LIMIT},
+        {"down_limit",         CMD_DOWN_LIMIT},
+        {"rtc_date",           CMD_RTC_DATE},
+        {"rtc_time",           CMD_RTC_TIME},
+        {"moisture",           CMD_MOISTURE},
+        {"motion",             CMD_MOTION},
+        {"activity_ratio",     CMD_ACTIVITY_RATIO},
+        {"activity_limit",     CMD_ACTIVITY_LIMIT},
+        {"recall_ratio",       CMD_RECALL_RATIO},
+        {"recall_timeout",     CMD_RECALL_TIMEOUT},
+        {"time_left",          CMD_TIME_LEFT},
+        {"call",               CMD_CALL},
+        {"energy_level",       CMD_ENERGY_LEVEL},
+        {"info",               CMD_INFO},
 };
 
 const uint8_t FLOAT_COMMANDS_LEN = 24;
@@ -171,6 +176,7 @@ public:
         }
         return false;
     }
+
     int32_t static getCode(char *name, const NetMap *map, uint8_t len)
     {
         for (uint8_t i = 0; i < len; i++) {
@@ -180,6 +186,7 @@ public:
         }
         return -1;
     }
+
     bool static isFloat(uint8_t cmd, const uint8_t map[], uint8_t len)
     {
         for (uint8_t i = 0; i < len; i++) {
