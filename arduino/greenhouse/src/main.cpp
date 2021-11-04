90c4b87bc4f7eaf43cf87947f4dc87103e622bac
Dmitriy Deryabin <dmitriy@ustisha.ru>
2021-11-04 18:12:14 +0700

    GREENHOUSE. Migrate to platformio. PCB layout.

--- arduino/greenhouse/src/main.cpp
+++ arduino/greenhouse/src/main.cpp
@@ -1,100 +1,83 @@
 //#define SERIAL_DEBUG
 
+#include <Arduino.h>
 #include <DebugLog.h>
-#include <SPI.h>
-#include <Wire.h>
-#include <Servo.h>
-#include <BME280Adapter.h>
-#include <InfoNet.h>
-#include <THPNet.h>
-#include <SmartNet.h>
-#include <AnalogReader.h>
-#include <ValueIntNet.h>
-#include <LoraNetReceive.h>
-#include <Vcc.h>
-#include <VccNet.h>
-#include <Relay.h>
-#include <TempController.h>
-#include <TempControllerNet.h>
 
-#ifdef IF_SERIAL_DEBUG
+#ifdef SERIAL_DEBUG
 
-int serial_putc(char c, FILE *) {
-    Serial.write(c);
-
-    return c;
-}
-
-void printf_begin(void) {
-    fdevopen(&serial_putc, 0);
-}
+#include <DebugFunc.h>
 
 #endif
 
-#define LORA_SS 10
-#define LORA_RESET 9
-#define LORA_DIO0 2
+#include <RF24Net.h>
+#include <OneWire.h>
+#include <DHTAdapter.h>
+#include <DS18B20Adapter.h>
+#include <AnalogReader.h>
+#include <Relay.h>
+#include <RelayNet.h>
+#include <TksM8.h>
+#include <Motion.h>
+#include <TempCtrlRelay.h>
+#include <LightController.h>
+#include <SmartNet.h>
+#include <TNet.h>
+#include <THNet.h>
+#include <ValueIntNet.h>
+#include <MotionNet.h>
+#include <TempCtrlRelayNet.h>
+#include <LightControllerNet.h>
 
-#define MOISTURE A2
-#define R1 4
-#define R2 6
-#define SERV1 5
-#define SERV2 7
+#define ONE_WIRE_BUS 3
+#define DHT22 5
+#define RADIO_RESET_CE 48
+#define RADIO_SS_CSN 49
 
-#define SENSOR_INTERVAL 120
+#define MOISTURE_1 63 //A9
+#define MOISTURE_2 65 //A11
+#define MOISTURE_3 67 //A13
+#define R1 20
+#define R2 22
+#define R3 24
+#define R4 26
+#define MOTION1 31
+#define TKS1_K1 12
+#define TKS1_K2 14
+#define TKS1_SW1 16
+#define TKS1_SW2 18
+#define TKS2_K1 8
+#define TKS2_K2 10
+#define TKS2_SW1 28
+#define TKS2_SW2 30
+
 // @todo 10 is debug value
-#define POLL_INTERVAL 10
+#define SENSOR_INTERVAL 30
+#define MOISTURE_INTERVAL 10
+#define POLL_INTERVAL 30
 
-const float vccCorrection = 1.0 / 1.0;
+const int memBase = 20;
 
-InfoNet *info;
 SmartNet *net;
-LoraNetReceive *loraNetReceive;
-BME280Adapter *bmeThp;
-THPNet *thpNet;
-Vcc *vcc;
-VccNet *vccNet;
-Relay *r1, *r2;
-TempController *tempController;
-TempControllerNet *tempControllerNet;
-Servo *serv1, *serv2;
-AnalogReader *moisture;
-ValueIntNet *moistureNet;
+RF24Net *rf24Net;
+THNet *thNet;
+TNet *tempNetT;
+DHTAdapter *dht22;
+DS18B20Adapter *dsTempT;
+Motion *m1;
+MotionNet *mNet1;
+Relay *r1, *r2, *r3, *r4;
+RelayNet *relayNet1;
+TksM8 *actuator1, *actuator2;
+LightController *lightController;
+LightControllerNet *lightControllerNet;
+TempCtrlRelay *tempController;
+TempCtrlRelayNet *tempControllerNet;
+AnalogReader *moisture1, *moisture2, *moisture3;
+ValueIntNet *moistureNet1, *moistureNet2, *moistureNet3;
 
-void onReceive(int packetSize) {
-    IF_SERIAL_DEBUG(printf_P(PSTR("[Main::onReceive] LoRa packet. Size %d\n"), packetSize));
+OneWire oneWire(ONE_WIRE_BUS);
+RF24 radio(RADIO_RESET_CE, RADIO_SS_CSN);
 
-    if (packetSize != PACKET_SIZE) {
-        return;
-    }
-
-    UInt sp(0);
-    UInt rp(0);
-    Long data(0);
-    Packet p{};
-
-    p.sender = (uint8_t) LoRa.read();
-    sp.b[0] = (uint8_t) LoRa.read();
-    sp.b[1] = (uint8_t) LoRa.read();
-    p.sp = sp.i;
-
-    p.receiver = (uint8_t) LoRa.read();
-    if (p.receiver != GREENHOUSE) {
-        return;
-    }
-    rp.b[0] = (uint8_t) LoRa.read();
-    rp.b[1] = (uint8_t) LoRa.read();
-    p.rp = rp.i;
-
-    p.cmd = (uint8_t) LoRa.read();
-    data.b[0] = (uint8_t) LoRa.read();
-    data.b[1] = (uint8_t) LoRa.read();
-    data.b[2] = (uint8_t) LoRa.read();
-    data.b[3] = (uint8_t) LoRa.read();
-    p.data = data.i;
-
-    net->commandReceived(p);
-}
 
 void setup(void) {
 #ifdef SERIAL_DEBUG
@@ -103,69 +86,146 @@ void setup(void) {
     IF_SERIAL_DEBUG(printf_P(PSTR("====== [DEBUG] ======\n")));
 #endif
 
-    IF_SERIAL_DEBUG(printf_P(PSTR("[Main] System started\n")));
+    IF_SERIAL_DEBUG(
+            printf_P(PSTR("[Main] System started. Ram: %d\n"), freeRAM()));
 
-    loraNetReceive = new LoraNetReceive(LORA_SS, LORA_RESET, LORA_DIO0);
-    loraNetReceive->onReceiveFunc(onReceive);
-    net = new SmartNet(GREENHOUSE);
-    info = new InfoNet(PORT_INFO, net);
+    EEPROM.setMemPool(memBase, EEPROMSizeATmega1280);
+
+    net = new SmartNet(GREENHOUSE, 9);
+    rf24Net = new RF24Net(net, GREENHOUSE, radio);
+    IF_SERIAL_DEBUG(printf_P(PSTR("[Main] Radio initialized\n")));
+
     // Info network started
-    info->sendCommandData(loraNetReceive, GATEWAY, PORT_HTTP_HANDLER, CMD_INFO, INFO_NETWORK_STARTED);
+    net->sendInfo(rf24Net, INFO_NETWORK_STARTED);
     IF_SERIAL_DEBUG(printf_P(PSTR("[Main] Network started\n")));
 
-    bmeThp = new BME280Adapter(Wire, 0x76);
-    bmeThp->setPollInterval(POLL_INTERVAL);
-    thpNet = new THPNet(PORT_BME280, net, bmeThp);
-    if (bmeThp->getStatus() < 0) {
-        // BME280 error
-        info->sendCommandData(loraNetReceive, GATEWAY, PORT_HTTP_HANDLER, CMD_INFO, INFO_ERROR_BME280);
-        IF_SERIAL_DEBUG(printf_P(PSTR("[Main] BME280 error\n")));
+    dsTempT = new DS18B20Adapter(&oneWire, 0);
+    dsTempT->setPollInterval(POLL_INTERVAL);
+    tempNetT = new TNet(net, PORT_18B20, 1, dsTempT);
+    if (!dsTempT->getStatus()) {
+        // DS18B20 error
+        net->sendInfo(rf24Net, INFO_ERROR_DS18B20);
+        IF_SERIAL_DEBUG(printf_P(PSTR("[Main] T DS18B20 error\n")));
     } else {
-        thpNet->addReceiver(loraNetReceive, GATEWAY, PORT_HTTP_HANDLER, CMD_TEMPERATURE, SENSOR_INTERVAL);
-        thpNet->addReceiver(loraNetReceive, GATEWAY, PORT_HTTP_HANDLER, CMD_HUMIDITY, SENSOR_INTERVAL);
-        thpNet->addReceiver(loraNetReceive, GATEWAY, PORT_HTTP_HANDLER, CMD_PRESSURE, SENSOR_INTERVAL);
+        dsTempT->read();
+        tempNetT->addReceiver(rf24Net, GATEWAY, PORT_HTTP_HANDLER, CMD_TEMPERATURE, SENSOR_INTERVAL);
     }
 
-    moisture = new AnalogReader(MOISTURE);
-    moistureNet = new ValueIntNet(PORT_VALUE, net, moisture);
-    moistureNet->addReceiver(loraNetReceive, GATEWAY, PORT_HTTP_HANDLER, CMD_MOISTURE, SENSOR_INTERVAL);
+    dht22 = new DHTAdapter(DHT22, DHT_TYPE_22);
+    dht22->setPollInterval(POLL_INTERVAL);
+    thNet = new THNet(net, PORT_DHT22, 2, dht22);
+    thNet->addReceiver(rf24Net, GATEWAY, PORT_HTTP_HANDLER, CMD_TEMPERATURE, SENSOR_INTERVAL);
+    thNet->addReceiver(rf24Net, GATEWAY, PORT_HTTP_HANDLER, CMD_HUMIDITY, SENSOR_INTERVAL);
+
+    moisture1 = new AnalogReader(MOISTURE_1);
+    // max 637 min 310
+    moisture1->setMapValues(310, 640, 100, 0);
+    moisture1->enableFilter();
+    moistureNet1 = new ValueIntNet(net, PORT_VALUE, 1, moisture1);
+    moistureNet1->addReceiver(rf24Net, GATEWAY, PORT_HTTP_HANDLER, CMD_MOISTURE, MOISTURE_INTERVAL);
+
+    moisture2 = new AnalogReader(MOISTURE_2);
+    // max 630 min 300
+    moisture2->setMapValues(300, 630, 100, 0);
+    moisture2->enableFilter();
+    moistureNet2 = new ValueIntNet(net, PORT_VALUE_2, 1, moisture2);
+    moistureNet2->addReceiver(rf24Net, GATEWAY, PORT_HTTP_HANDLER, CMD_MOISTURE, MOISTURE_INTERVAL);
+
+    moisture3 = new AnalogReader(MOISTURE_3);
+    // max 630 min 305
+    moisture3->setMapValues(305, 630, 100, 0);
+    moisture3->enableFilter();
+    moistureNet3 = new ValueIntNet(net, PORT_VALUE_3, 1, moisture3);
+    moistureNet3->addReceiver(rf24Net, GATEWAY, PORT_HTTP_HANDLER, CMD_MOISTURE, MOISTURE_INTERVAL);
 
     r1 = new Relay(R1);
     r2 = new Relay(R2);
-    serv1 = new Servo();
-    serv1->attach(SERV1);
-    serv2 = new Servo();
-    serv2->attach(SERV2);
-    tempController = new TempController(bmeThp, 26.0, 27.0);
-    // @todo 5 is debug value.
-    tempController->setTimeout(5);
-    tempController->addRelay(r1, 0, true);
-    tempController->addRelay(r2, 1, true, 0.5, 0.1);
-    tempController->addServo(serv1, 0, false, 160, 2.4);
-    tempController->addServo(serv2, 1, false, 160, 1.8);
-    tempControllerNet = new TempControllerNet(PORT_TEMP_CONTROLLER, net, tempController);
-    tempController->addNet(loraNetReceive, tempControllerNet, GATEWAY, PORT_HTTP_HANDLER);
+    r3 = new Relay(R3);
+    r4 = new Relay(R4);
+    r1->off();
+    r2->off();
+    r3->off();
+    r4->off();
 
-    vcc = new Vcc(vccCorrection);
-    vccNet = new VccNet(PORT_VALUE, net, vcc);
-    vccNet->addReceiver(loraNetReceive, GATEWAY, PORT_HTTP_HANDLER, CMD_VCC, SENSOR_INTERVAL);
+    relayNet1 = new RelayNet(net, PORT_RELAY_1, 1, r1);
+    r1->addNet(rf24Net, relayNet1, GATEWAY, PORT_HTTP_HANDLER);
 
-    // Info setup completed
-    info->sendCommandData(loraNetReceive, GATEWAY, PORT_HTTP_HANDLER, CMD_INFO, INFO_SETUP_COMPLETED);
-    IF_SERIAL_DEBUG(printf_P(PSTR("[Main] Setup completed\n")));
+    actuator1 = new TksM8(TKS1_K1, TKS1_K2, TKS1_SW1, TKS1_SW2, 15);
+    actuator2 = new TksM8(TKS2_K1, TKS2_K2, TKS2_SW1, TKS2_SW2, 15);
+    actuator1->off();
+    actuator2->off();
+
+    m1 = new Motion(MOTION1, 5, 1);
+    mNet1 = new MotionNet(net, PORT_MOTION_1, 1, m1);
+    m1->addNet(rf24Net, mNet1, GATEWAY, PORT_HTTP_HANDLER);
+    lightController = new LightController();
+    lightController->setTimeout(30 * 1000);
+    lightController->addRelay(r3);
+    lightController->addMotion(m1);
+    lightControllerNet = new LightControllerNet(net, PORT_LIGHT_CTRL_00, 1, lightController);
+    lightController->addNet(rf24Net, lightControllerNet, GATEWAY, PORT_HTTP_HANDLER);
+    lightController->sendValues();
+
+    tempController = new TempCtrlRelay(dht22, 4, 28.0, 30.0);
+    tempController->addRelay(r2,
+                             0,
+                             TempCtrlRelay::TYPE_TEMPERATURE
+                             | TempCtrlRelay::TYPE_BELOW_DOWN_LIMIT
+                             | TempCtrlRelay::TYPE_ABOVE_DOWN_LIMIT,
+                             1,
+                             1);
+    tempController->addRelay(r4,
+                             1,
+                             TempCtrlRelay::TYPE_TEMPERATURE
+                             | TempCtrlRelay::TYPE_BELOW_DOWN_LIMIT
+                             | TempCtrlRelay::TYPE_ABOVE_DOWN_LIMIT,
+                             3,
+                             2);
+    tempController->addRelay(actuator1,
+                             2,
+                             TempCtrlRelay::TYPE_TEMPERATURE
+                             | TempCtrlRelay::TYPE_ABOVE_UP_LIMIT
+                             | TempCtrlRelay::TYPE_BELOW_UP_LIMIT,
+                             5,
+                             5);
+    tempController->addRelay(actuator2,
+                             3,
+                             TempCtrlRelay::TYPE_TEMPERATURE
+                             | TempCtrlRelay::TYPE_ABOVE_UP_LIMIT
+                             | TempCtrlRelay::TYPE_BELOW_UP_LIMIT,
+                             0,
+                             5);
+
+    tempController->initDone();
+    tempControllerNet = new TempCtrlRelayNet(net, PORT_TEMP_CTRL, 1, tempController);
+    tempController->addNet(rf24Net, tempControllerNet, GATEWAY, PORT_HTTP_HANDLER);
+    tempController->sendValues();
+
+    net->sendInfo(rf24Net, INFO_SETUP_COMPLETED);
+    IF_SERIAL_DEBUG(
+            printf_P(PSTR("[Main] Setup completed. Ram: %d\n"), freeRAM()));
 }
 
 void loop(void) {
-#ifdef SERIAL_DEBUG
-    Serial.flush();
-#endif
-    if (bmeThp->getStatus()) {
-        bmeThp->tick();
-    }
+    rf24Net->tick();
 
-    thpNet->tick();
-    vccNet->tick();
-    moistureNet->tick();
-    tempController->tick();
+    dsTempT->tick();
+    tempNetT->tick();
+    dht22->tick();
+    thNet->tick();
+
+    relayNet1->tick();
+    moistureNet1->tick();
+    moistureNet2->tick();
+    moistureNet3->tick();
+
+    m1->tick();
+    mNet1->tick();
+    lightController->tick();
+    lightControllerNet->tick();
+
+    actuator1->tick();
+    actuator2->tick();
+    tempController->tick(0);
     tempControllerNet->tick();
 }
